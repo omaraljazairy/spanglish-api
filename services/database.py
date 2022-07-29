722307ddfe70f79b269370707e98ac7c43ff9cdc
Omar Aljazairy <o.aljazairy@amsterdam.nl>
2022-07-29 22:41:41 +0200

    modified the router response

--- services/database.py
+++ services/database.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine, engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DB_HOST = None #os.getenv("DB_HOST", None)
+DB_HOST = os.getenv("DB_HOST", None)
 DB_USER = os.getenv("DB_USER", None)
 DB_PASSWORD = os.getenv("DB_PASSWORD", None)
 POSTGRES_USER = os.getenv("POSTGRES_USER", None)
@@ -21,7 +21,7 @@ SQLALCHEMY_DATABASE_URL = MYSQL_URL if DB_HOST else SQLITE_URL
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={
-        "check_same_thread": False
+        # "check_same_thread": False
     }
 )
 # add echo=True to the create_engine to echo the db queries
